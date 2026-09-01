d0f8fd2e60f4904c2392e526991f98f9b67b4fc7
Aleksandar Todorovic <71438536+doca88@users.noreply.github.com>
2026-09-01 09:56:43 +0000

    Configuration

--- SecurityMasterService/Program.cs
+++ SecurityMasterService/Program.cs
@@ -33,6 +33,12 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("/test", (int id) =>
+{
+    if(id > 0)
+   return "PRC";
+   return "PRC2";
+}).WithName("GetTest");
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
